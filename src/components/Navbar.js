d18f9dee91ccda74b5caf52cef5c998cd81dac41
vincenthnguyen <vinhhnguyen@cmail.carleton.ca>
2020-06-12 10:55:46 -0400

    alignment fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
 					<span className='navbar-toggler-icon' />
 				</button>
 				<div className='collapse navbar-collapse' id='mobileNav'>
-					<ul className='navbar-nav mr-auto'>
+					<ul className='navbar-nav ml-auto'>
 						<li className='nav-item'>
 							<Link to={'/'} className={'nav-link'}>
 								Home
